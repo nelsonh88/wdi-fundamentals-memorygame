e4b0c86db10a42c760b7c1572be4f31070c99c53
Nelson Huezo <nellyinmybelly1@Nelly-in-my-Belly.local>
2018-01-15 14:43:10 -0500

    unit 10 changes

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		"rank":"queen", 
+		"suit":"hearts",
+		"cardImage":"../images/queen-of-hearts.png"
+	},
+	{
+		"rank":"queen", 
+		"suit":"diamonds",
+		"cardImage":"../images/queen-of-diamonds.png"
+	},
+	{
+		"rank":"king", 
+		"suit":"hearts",
+		"cardImage":"../images/king-of-hearts.png"
+	},
+	{
+		"rank":"king", 
+		"suit":"diamonds",
+		"cardImage":"../images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 
@@ -14,10 +35,12 @@ function checkForMatch(){
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
